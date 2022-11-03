2794a6cb07ee499967f3f29796322e010d443358
TarikAydin29 <you@example.com>
2022-11-03 10:42:56 +0300

    DbContext,Connections

---
+++ EF_Entities_TA/Data/RecordStoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EF_Entities_TA.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EF_Entities_TA.Data
+{
+    public class RecordStoreDbContext : DbContext
+    {
+        public RecordStoreDbContext(DbContextOptions<RecordStoreDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ EF_Entities_TA/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Entities_TA.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [MaxLength(30)]
+        [Index(IsUnique =true)]
+        [Unique]
+        public string UserName { get; set; }
+        [MaxLength(12)]
+        public string Password { get; set; }
+    }
+}
