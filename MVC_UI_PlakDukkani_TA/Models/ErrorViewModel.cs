a36e6371f13d62b22e90f8a786438dcb9446f165
TarikAydin29 <you@example.com>
2022-11-03 09:35:23 +0300

    Add project files.

---
+++ MVC_UI_PlakDukkani_TA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_UI_PlakDukkani_TA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
